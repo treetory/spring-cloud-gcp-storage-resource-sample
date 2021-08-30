541d8f18e49f1aa756012695d1220d2c8a76fc24
inhwan-chun <inhwan.chun@ngenebio.com>
2021-08-30 11:50:39 +0900

    fix the redirect api

--- src/main/java/com/example/GcsController.java
+++ src/main/java/com/example/GcsController.java
@@ -14,6 +14,7 @@ import org.springframework.web.multipart.MultipartFile;
 
 import java.io.File;
 import java.io.IOException;
+import java.net.URISyntaxException;
 import java.net.URL;
 import java.util.List;
 
@@ -225,12 +226,17 @@ public class GcsController {
     }
 
     @GetMapping("/url")
-    public ResponseEntity<?> getSignedURL(@RequestParam String fileName) throws IOException {
+    public ResponseEntity<?> getSignedURL(@RequestParam String fileName) throws IOException, URISyntaxException {
 
         URL signedURL = gcsService.getSignedURL(fileName);
+
+        HttpHeaders httpHeaders = new HttpHeaders();
+        httpHeaders.setLocation(signedURL.toURI());
+
         return ResponseEntity
-                .status(HttpStatus.OK)
-                .body(signedURL);
+                .status(HttpStatus.TEMPORARY_REDIRECT)
+                .headers(httpHeaders)
+                .build();
     }
 
     @GetMapping("/{fileName}/download")
